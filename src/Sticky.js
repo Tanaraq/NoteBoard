202c428e4686b15c831e434239b5c834289f482f
Annabel <annabelkooistra@outlook.com>
2022-10-29 14:42:44 +0200

    add random angle, top and color to the stickies

--- src/Sticky.js
+++ src/Sticky.js
@@ -2,8 +2,28 @@ import React from "react";
 import { Note, Title, Text, Bin } from "./styles";
 
 export const Sticky =({data, removeSticky}) =>{
+    
+    const randomTop= () => {
+        return (Math.floor(Math.random() * 21) -10)+"px" ; //need a random number between -5 and +5, with px attached
+    } 
+    console.log(randomTop()); //works!
+
+    const randomAngle= () => {
+        return (Math.floor(Math.random() * 9) -4)+"deg" ; //need a random number between -4 and +4, with deg attached
+    }
+    
+    const randomColor = () =>{
+        const colorArray = [ '#ffc', '#ffc', '#cfc', '#fcf', '#cff']; // 4colors, but I want more yellow, so #ffc is in the array twice
+        let random=(Math.floor(Math.random()*5));
+        return colorArray[random];
+    }
+        
     return (
-        <Note>
+        <Note   
+            angle={randomAngle()}
+            top={randomTop()}
+            color={randomColor()}
+        >
             <Title>{data.title}</Title>
             <Text>{data.text}</Text>
             <Bin onClick={() => removeSticky(data.id)}
--- src/styles.js
+++ src/styles.js
@@ -1,10 +1,6 @@
 import styled from "@emotion/styled";
 import Cork from './cork_background.jpg';
 
-const randomTop= Math.floor(Math.random() * 11) -5 ; //need a random number between -5 and +5
-const randomAngle= Math.floor(Math.random() * 9) -4 ; //need a random number between -4 and +4
-//const colorArray = [ #ffc, #ffc, #cfc, #fcf, #cff]; // 4colors, but i want more yellow, so #ffc is in the array twice
-
 export const Board = styled.div`
     width: 98vw;
     height: 98vh;
@@ -34,13 +30,14 @@ export const Note = styled.div`
     min-height: 9em;
     max-width: 20vw;
     max-height: 70vh;
-    top: ${randomTop};
-    background-color: #ffc;
+    position: relative;
+    top: ${(props) => props.top}; 
+    background-color: ${(props) => props.color}; 
     margin: 0.5em;
     padding: 1em;
     font-family: 'Caveat', cursive;
     box-shadow: 5px 5px 7px rgba(33, 33, 33, .7);
-    transform: rotate(${randomAngle}deg);
+    transform: rotate(${(props) => props.angle});
 `;
 
 export const Title = styled.h2`
